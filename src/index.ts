538a1b30f80e531ced27879c6032234d9cc87853
jeonghoj <jeonghoj94@gmail.com>
2020-01-02 02:58:43 +0900

    generate code with yeoman express-no-stress-typescript

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './config/env';
+import Server from './common/server';
+import routes from './routes';
+
+const port = parseInt(process.env.PORT);
+export default new Server().router(routes).listen(port);
